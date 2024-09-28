dd2a876215659cfdcd01ce4d2cb9bcd7b96b7450
taeyungithub <rlaxodbs629@naver.com>
2024-09-28 14:18:03 +0900

    블로그 화면 구성

---
+++ src/main/java/spring/springbootdeveloper/controller/BlogViewController.java
@@ -0,0 +1,50 @@
+package spring.springbootdeveloper.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import spring.springbootdeveloper.domain.Article;
+import spring.springbootdeveloper.dto.ArticleListViewResponse;
+import spring.springbootdeveloper.dto.ArticleViewResponse;
+import spring.springbootdeveloper.service.BlogService;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article",new ArticleViewResponse(article));
+        }
+        return "newArticle";
+    }
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO article (title, content)
-VALUES ('제목 1', '내용1');
-INSERT INTO article (title, content)
-VALUES ('제목 2', '내용2');
-INSERT INTO article (title, content)
-VALUES ('제목 3', '내용3');
+INSERT INTO article (title, content,created_at,updated_at)
+VALUES ('제목 1', '내용1',NOW(),NOW());
+INSERT INTO article (title, content,created_at,updated_at)
+VALUES ('제목 2', '내용2',NOW(),NOW());
+INSERT INTO article (title, content,created_at,updated_at)
+VALUES ('제목 3', '내용3',NOW(),NOW());
